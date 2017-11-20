c1fe9ce44b21c78757474c3a6855dbfdc91b29c1
Arpit Jain <arpit_jain@live.com>
2017-11-20 22:40:39 +0530

    index page responsive, animation, beautification

--- laundrywalla/js/script.js
+++ laundrywalla/js/script.js
@@ -1,17 +1,36 @@
+$(document).ready(function() {
+    AOS.init();
+});
+
 /* When user clicks the Icon */
 $(".nav-toggle").click(function() {
     $(this).toggleClass("active");
-    $(".overlay-boxify").toggleClass("open");
+    if ($(".overlay-boxify").hasClass('open')) {
+      $(".overlay-boxify").removeClass("open");
+    } else {
+      $(".overlay-boxify").addClass("open");
+    }
+    // $(".overlay-boxify").toggleClass("open");
 });
 
 /* When user clicks a link */
 $(".overlay ul li a").click(function() {
-    $(".nav-toggle").toggleClass("active");
-    $(".overlay-boxify").toggleClass("open");
+    $(".nav-toggle").removeClass("active");
+    $(".overlay-boxify").removeClass("open");
 });
 
 /* When user clicks outside */
 $(".overlay").click(function() {
-    $(".nav-toggle").toggleClass("active");
-    $(".overlay-boxify").toggleClass("open");
+    $(".nav-toggle").removeClass("active");
+    $(".overlay-boxify").removeClass("open");
+});
+
+/***********************************
+ Date picker
+ ***********************************/
+
+$(function() {
+    $( "#pick-up-datepicker" ).datepicker();
+    $( "#drop-datepicker" ).datepicker();
+    $('[data-toggle="popover"]').popover({ trigger: "hover" });
 });
